56bdaf9e9eec732a0fb121ce6f86b8ee5c9551fd
hasnat18 <hsntkhan1614@gmail.com>
2025-07-04 19:23:25 +0500

    new

---
+++ public/build/assets/badge-Dv7Jyjks.js
@@ -0,0 +1,6 @@
+import{d as o}from"./authenticated-layout-GTORCj1n.js";import{j as n}from"./app-IiSPVizZ.js";import{c as s,b as i}from"./button-DZ5k6_p9.js";/**
+ * @license lucide-react v0.378.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=o("Ellipsis",[["circle",{cx:"12",cy:"12",r:"1",key:"41hilf"}],["circle",{cx:"19",cy:"12",r:"1",key:"1wjl8i"}],["circle",{cx:"5",cy:"12",r:"1",key:"1pcz8c"}]]),a=i("inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",{variants:{variant:{default:"border-transparent bg-primary text-primary-foreground hover:bg-primary/80",secondary:"border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",destructive:"border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",outline:"text-foreground"}},defaultVariants:{variant:"default"}});function l({className:r,variant:e,...t}){return n.jsx("div",{className:s(a({variant:e}),r),...t})}export{l as B,f as E};
